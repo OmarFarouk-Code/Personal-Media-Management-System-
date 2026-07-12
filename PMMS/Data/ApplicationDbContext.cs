6a11b710ac0d232db18c91f3887a6acdadb50ec7
OmarFarouk-Code <omarrfaroukk1212@gmail.com>
2026-07-12 12:42:33 +0300

    Installed Entity Framework

---
+++ PMMS/Data/ApplicationDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PMMS.Models;
+
+namespace PMMS.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        // This constructor passes configuration (like your connection string) to the base DbContext
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        // These DbSets represent your actual SQL tables
+        public DbSet<MediaItem> MediaItems { get; set; }
+        public DbSet<WatchLog> WatchLogs { get; set; }
+    }
+}
